e6fae4c6202eee18587dd78e31022f5ff1e88bf5
maya <C00217557@itcarlow.ie>
2021-10-27 14:01:44 +0100

    Computing Technologies Subpage One complete

--- app.py
+++ app.py
@@ -11,39 +11,34 @@ def index():
 
 @app.get("/showpersonalpage")
 def displayPersonal():
-    """
-        Retrieve the form.html file from the hard drive, and send it to the browser
-    """
     return render_template("personalPage.html", title="Personal Page", heading="This is a page about me",)
 
 @app.get("/showCVpage")
 def displayCV():
-    """
-        Retrieve the form.html file from the hard drive, and send it to the browser
-    """
     return render_template("CV.html", title="CV Page", heading="This is my CV",)
 
 @app.get("/showtechnologiespage")
 def displayTechnologies():
-    """
-        Retrieve the form.html file from the hard drive, and send it to the browser
-    """
     return render_template("computingTechnologies.html", title="Computing Technology", heading="Please fill in this form",)
+
+@app.get("/showTechSubpage1")
+def displaySub1():
+    return render_template("computingTechnologiesSubpageOne.html", title="CV Page", heading="This is my CV",)
+
+@app.get("/showTechSubpage2")
+def displaySub2():
+    return render_template("computingTechnologiesSubpageTwo.html", title="CV Page", heading="This is my CV",)
+
+@app.get("/showTechSubpage3")
+def displaySub3():
+    return render_template("computingTechnologiesSubpageThree.html", title="CV Page", heading="This is my CV",)
 
 @app.get("/showinterestspage")
 def displayinterests():
-    """
-        Retrieve the form.html file from the hard drive, and send it to the browser
-    """
     return render_template("interests.html", title="Interests", heading="These are my non-computing related interests",)
 
 @app.post("/processform")
 def showPersonal():
-    """
-        retrieve the data from the html form, then save it to a disk file, then respond to the waiting browser.
-
-        the following are expected: first, last, DOB
-    """
     theEmail = request.form["email"]
     theMessage = request.form["message"]
 
